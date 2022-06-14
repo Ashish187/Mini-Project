d7c36e4d8c1ff58113badd58876163d07503576f
Ashish187 <ashishanand16086@gmail.com>
2022-06-14 12:22:41 +0530

    price

--- public/js/price.js
+++ public/js/price.js
@@ -1,8 +1,36 @@
-var output = document.getElementById('amountToRaise')
-
-var slider = document.getElementById('myRange').oninput = function(){
-    var value = (this.value-this.min)/(this.max-this.min)*100
-    this.style.background = 'linear-gradient(to right,#6b8dff 0%,#ff2a5f '+value+'%,#fff ' + value + '%, #fff 100%)'
-
-    output.innerHTML = this.value
+function updateChange(val){
+    document.getElementById('amountToRaise').value = val
 }
+
+var calci = document.getElementById('amountToRaise').value
+
+var total = document.querySelector('.total')
+var calculate = document.createElement('h3')
+calculate.classList.add('calculate')
+
+var one_col2 = document.querySelector('.one-col2')
+var raise1 = document.createElement('h4')
+raise1.classList.add('raise1')
+
+var plat1 = document.createElement('h4')
+plat1.classList.add('plat1')
+
+var pay1 = document.createElement('h4')
+pay1.classList.add('pay1');
+
+raise1.innerText = `${calci}`
+plat1.innerText = `${0}`
+
+
+let x = (calci*2)/100;
+let y = x+calci
+calculate.innerHTML = `${y}`
+pay1.innerText = `${x}`
+
+
+
+one_col2.appendChild(raise1)
+one_col2.appendChild(plat1)
+one_col2.appendChild(pay1)
+
+total.appendChild(calculate)
