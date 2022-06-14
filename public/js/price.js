3b5ba7f3781352be6abf44ac6fc6ea2345835bff
Ashish187 <ashishanand16086@gmail.com>
2022-06-14 20:27:35 +0530

    pricing completed

--- public/js/price.js
+++ public/js/price.js
@@ -1,8 +1,8 @@
 function updateChange(val){
     document.getElementById('amountToRaise').value = val
 }
-
-let calci = document.getElementById('amountToRaise').value
+var btn = document.querySelector('.btn2')
+// let calci = document.getElementById('amountToRaise').value
 var total = document.querySelector('.total')
 var calculate = document.createElement('h3')
 calculate.classList.add('calculate')
@@ -17,18 +17,39 @@ plat1.classList.add('plat1')
 var pay1 = document.createElement('h4')
 pay1.classList.add('pay1');
 
-raise1.innerHTML = `${calci}`
+// raise1.innerHTML = `${calci}`
 
-plat1.innerText = `${0}`
+// plat1.innerText = `${0}`
 
-let a = (calci*2)/100;
-let b=parseInt(calci)
-let c=parseInt(a+b)
-console.log(c);
+// let a = (calci*2)/100;
+// let b=parseInt(calci)
+// let c=parseInt(a+b)
+// console.log(c);
 
 
-pay1.innerText = `${a}`
-calculate.innerHTML = `${c}`
+// pay1.innerText = `${a}`
+// calculate.innerHTML = `${c}`
+
+btn.addEventListener('click',()=>{
+    let calci = document.getElementById('amountToRaise').value
+    raise1.innerHTML = `${calci}`
+
+    plat1.innerText = `${0}`
+
+    let a = (parseInt(calci)*2)/100;
+    let b=parseInt(calci)
+    let c=parseInt(a+b)
+
+    pay1.innerText = `${a}`
+    calculate.innerHTML = `₹${c}`
+
+    document.querySelector('.calculation').style.display = 'flex'
+
+    console.log(c);
+    console.log(a);
+    console.log(calci);
+})
+
 
 one_col2.appendChild(raise1)
 one_col2.appendChild(plat1)
